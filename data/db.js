dc66d0d934f13e4ccbbb99ea972e047a9f78fe02
GlowingPiranha <ikimus01@gmail.com>
2025-09-22 01:56:10 +0200

    Update db.js

--- data/db.js
+++ data/db.js
@@ -5,11 +5,11 @@ dotenv.config();
 
 // * connessione al database
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "1234",
-  database: process.env.DB_NAME || "db_movies",
-  port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 
